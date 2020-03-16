b4b043d759bc1f3096160a857aeecc2785acf47a
gadapi <correogary@outlook.com>
2020-03-16 13:46:00 -0500

    Update Accordion.js

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -38,7 +38,7 @@ export default class Accordion extends React.Component {
                 <ChevronDown />
               </h2>
               <div className={'description'}>
-                {item.description} <br />
+                {item.body} <br />
                 {item.link && (
                   <div href={item.link} className="button">
                     {item.linkTitle}
