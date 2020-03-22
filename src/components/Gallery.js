7c9df4ffef54d4ce4d46faa6ba9c14b26c86ecc1
gadapi <correogary@outlook.com>
2020-03-22 08:01:54 -0500

    gallery

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -57,7 +57,7 @@ export default class Gallery extends Component {
         }
       )
 
-  componentDidMount() {
+    componentDidCatch() {
     const { images } = this.props,
       maxCount = images.length
     let loopCount = 1
