2f694d0a1b4c4d43244ef0a1b39fca8acbaedd7a
gadapi <correogary@outlook.com>
2020-03-25 16:59:16 -0500

    single post date

--- src/templates/SinglePost.js
+++ src/templates/SinglePost.js
@@ -122,7 +122,7 @@ export const pageQuery = graphql`
         title
         template
         subtitle
-        date formatString("DD-MM-YYYY")
+        date
         categories {
           category
         }
