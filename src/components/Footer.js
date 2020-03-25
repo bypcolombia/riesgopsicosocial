5c25d46f2d1708b1fe8fdfc482b91471a32d66dc
gadapi <correogary@outlook.com>
2020-03-25 11:28:05 -0500

    varios 25 marzo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import InstagramFeed from './InstagramFeed'
 import './Footer.css'
-import SVGIcon from '../components/SVGIcon'
+
 
 export default () => (
   <div>
   <div className="social-container">
   <a href="https://www.facebook.com/bienestaryproductividad/"><img className="img-social" src="/images/facebook.svg" alt="Facebook Bienestar y Productividad"/> </a>
   <a href="https://www.youtube.com/playlist?list=PLya0MpsDrUt20BFwFKPLyoiU5cWSszc4k"><img className="img-social" src="/images/youtube.svg" alt="Youtube Bienestar y Productividad"/> </a>
-  <a href="https://www.instagram.com/"><img className="img-social" src="/images/instagram.svg" alt="Facebook Bienestar y Productividad"/> </a>
-  <a href="https://www.linkedin.com/"><img className="img-social" src="/images/linkedin.svg" alt="Facebook Bienestar y Productividad"/> </a>
+  <a href="https://instagram.com/bienestaryproductividad/"><img className="img-social" src="/images/instagram.svg" alt="Facebook Bienestar y Productividad"/> </a>
+  <a href="https://www.linkedin.com/company/16260312/"><img className="img-social" src="/images/linkedin.svg" alt="Facebook Bienestar y Productividad"/> </a>
   </div>
   <h2 className="taCenter">
       Síguenos en{' '}
